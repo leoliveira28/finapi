ecccec5554b24bb712cfa9f644fa2630b190bef3
Leandro Pimentel <leandropimentel2011@gmail.com>
2022-05-18 21:44:20 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const { response } = require('express');
+const express = require('express');
+const { v4: uuidv4 } = require('uuid')
+
+const app = express();
+
+app.use(express.json())
+
+const customers = [];
+
+app.post("/account", (req, res) => {
+    const { cpf, name } = req.body;
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+    if(customerAlreadyExists) {
+        return res.status(400).json({error: 'Customer already exists'})
+    }
+
+    const id = uuidv4();
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    })
+
+    return res.status(201).send()
+
+    
+});
+
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+    const customer = customers.find(customer => customer.cpf === cpf);
+    if(!customer) {
+        return response.status(400).json({error: 'Customer not found'}); 
+    }
+
+    request.customer = customer;
+    return next();
+}
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+ 
+    const { customer } = request;
+    return response.json(customer.statement);
+})
+
+app.listen(3333);
